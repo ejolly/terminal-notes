597f3a2cecdef7723fb9cd847123ed31adfa211d
ejolly <eshin.jolly@gmail.com>
2020-06-28 15:40:38 -0400

    Added -f flag to print file location. Better help, code comments, swapped os for pathlib.

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Super simple command line program for managing "what you were working on" style notes in the terminal.  
 Inspired by [doing](https://github.com/ttscoff/doing) but even simpler.  
-Uses the [click](https://click.palletsprojects.com/en/7.x/) Python library and is specifically setup to make the `note` command available to your shell following [these directions](https://click.palletsprojects.com/en/7.x/setuptools/) 
+Uses the [click](https://click.palletsprojects.com/en/7.x/) Python library and is specifically setup to make the `note` command available to your shell following [these directions](https://click.palletsprojects.com/en/7.x/setuptools/)  
+**Note:** There is no "archive" function. Removing a note always _deletes_ it from storage!  
+
+## Usage
+
+Type `note --help` to see usage options
 
 ## Auto prompt of pending items on new terminal launch
 
--- note.py
+++ note.py
@@ -3,25 +3,39 @@
 Easily store and view recent notes
 """
 
-import os
+from pathlib import Path
 import click
+
+file_path = "/Users/Esh/Documents/.click_notes.txt"
+p = Path(file_path)
+
+if not p.exists():
+    print("No notes file found...creating")
+    p.touch()
 
 
 @click.command()
 @click.argument("note", nargs=-1)
-@click.option("-a", "archive", type=int, help="Archive a specific note by its number")
-@click.option("-A", "archive_all", is_flag=True, help="Archive all notes")
-def note_writer(archive, archive_all, note):
-    """Add or show (no args) notes"""
+@click.option("-f", "filepath", is_flag=True, help="Print location to notes file")
+@click.option("-a", "-d", "archive", type=int, help="Remove a specific note by its number (0 indexed)")
+@click.option("-A", "archive_all", is_flag=True, help="Remove all notes")
+def note_writer(archive, archive_all, note, filepath):
+    """Simple program to add or show notes from a file. Works entirely through command flags. Removed notes are deleted from the file so there is no history of completed items!"""
+    if filepath:
+        # If -f is passed just print the location to the file and exit
+        click.echo(f"Notes file is here: {p.absolute()}")
+        return
     if not note:
         with open("/Users/Esh/Documents/.click_notes.txt", "r") as f:
             lines = f.readlines()
+        # If no note text is passed and no flags are provided just print all notes
         if not isinstance(archive, int) and not archive_all:
             if lines:
                 click.echo("📌 You were doing...")
                 for i, line in enumerate(lines):
                     print(f"{i}) {line}", end="")
         else:
+            # If no note text is passed but archival flags are passed, remove the requested lines
             with open("/Users/Esh/Documents/.click_notes.txt", "w") as f:
                 if isinstance(archive, int):
                     for i, line in enumerate(lines):
@@ -31,6 +45,7 @@ def note_writer(archive, archive_all, note):
                     f.truncate()
                     click.echo("All notes cleared!")
     else:
+        # If note text is passed ignore all other flags and append to the notes file
         with open("/Users/Esh/Documents/.click_notes.txt", "a") as f:
             to_write = f"{' '.join(note)}\n"
             f.write(to_write)
